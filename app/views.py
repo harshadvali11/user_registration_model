2045acd05ebc371fc14399bfe81485592fe5819c
harshadvali11 <harshadvali1431@gmail.com>
2023-01-06 18:38:30 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from app.forms import *
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib.auth import login,authenticate,logout
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 # Create your views here.
 from django.core.mail import send_mail
+
 def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
     return render(request,'home.html')
+
+
+
+
 def registration(request):
     uf=UserForm()
     pf=ProfileForm()
@@ -32,8 +44,33 @@ def registration(request):
     return render(request,'registration.html',d)
 
 
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['username']        
+        password=request.POST['password']
+        user=authenticate(username=username,password=password)
+        if user and user.is_active:
+            login(request,user)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
 
 
 
-\
+
+    return render(request,'user_login.html')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
+
+
+
+
+
+
+
+
+
 
--- project32/urls.py
+++ project32/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('registration/',registration,name='registration'),
     path('home/',home,name='home'),
+    path('user_login/',user_login,name='user_login'),
+    path('user_logout/',user_logout,name='user_logout'),
 
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
